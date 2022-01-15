c2bec06ee7275f3f07e2af76a1d4c99071af876e
Harin <master.harin@gmail.com>
2022-01-15 12:06:21 -0800

    frontend setup

---
+++ frontend/src/screens/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from 'antd';
+
+function Login() {
+    return (
+        <div>
+        <Button type="primary">Button</Button>
+      </div>
+    );
+  }
+
+export default Login;
