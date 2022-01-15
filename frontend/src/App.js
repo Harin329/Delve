e4e9dba4cb5ce3ab6b750f6cd039fb39a3b841eb
Harin <master.harin@gmail.com>
2022-01-15 13:00:05 -0800

    home page stuff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
 import Login from './screens/Login';
+import Home from './screens/Home';
 
 function App() {
-  const loggedIn = false;
+  const loggedIn = true;
   if (loggedIn) {
-
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return Home();
   }
 
   return Login();
---
+++ frontend/src/screens/Home.js
@@ -0,0 +1,170 @@
+import React from 'react';
+import '../App.css';
+import { Layout, Input, List, Card, Row } from 'antd';
+
+function Home() {
+    const { Header, Sider, Content } = Layout;
+    const { Search } = Input;
+
+    const onSearch = value => console.log(value);
+
+    const data = [
+        {
+            title: 'Title 1',
+        },
+        {
+            title: 'Title 2',
+        },
+        {
+            title: 'Title 3',
+        },
+        {
+            title: 'Title 4',
+        },
+        {
+            title: 'Title 1',
+        },
+        {
+            title: 'Title 2',
+        },
+        {
+            title: 'Title 3',
+        },
+        {
+            title: 'Title 4',
+        },
+        {
+            title: 'Title 1',
+        },
+        {
+            title: 'Title 2',
+        },
+        {
+            title: 'Title 3',
+        },
+        {
+            title: 'Title 4',
+        },
+        {
+            title: 'Title 1',
+        },
+        {
+            title: 'Title 2',
+        },
+        {
+            title: 'Title 3',
+        },
+        {
+            title: 'Title 4',
+        },
+        {
+            title: 'Title 1',
+        },
+        {
+            title: 'Title 2',
+        },
+        {
+            title: 'Title 3',
+        },
+        {
+            title: 'Title 4',
+        },
+        {
+            title: 'Title 1',
+        },
+        {
+            title: 'Title 2',
+        },
+        {
+            title: 'Title 3',
+        },
+        {
+            title: 'Title 4',
+        },
+        {
+            title: 'Title 3',
+        },
+        {
+            title: 'Title 4',
+        },
+        {
+            title: 'Title 1',
+        },
+        {
+            title: 'Title 2',
+        },
+        {
+            title: 'Title 3',
+        },
+        {
+            title: 'Title 4',
+        }
+    ];
+
+    return (
+        <Layout className='App'>
+            <Sider className='Sider' style={{ backgroundColor: 'white' }}>
+                <Row align='middle' justify='center'>
+                    Delve
+                </Row>
+                <Row>
+                    <Row>
+                        Profile
+                    </Row>
+                    <Row>
+                        Post a Study
+                    </Row>
+                    <Row>
+                        Post Results
+                    </Row>
+
+
+                </Row>
+                <Row>
+                    <Row>
+                        All
+                    </Row>
+                    <Row>
+                        Clinical Trials
+                    </Row>
+                    <Row>
+                        Environmental
+                    </Row>
+                    <Row>
+                        Civic
+                    </Row>
+                    <Row>
+                        Psychology
+                    </Row>
+                    <Row>
+                        Science
+                    </Row>
+                </Row>
+            </Sider>
+            <Layout>
+                <Header>
+                    <Row align='middle' justify='center'>
+                        <Search placeholder="input search text" onSearch={onSearch} style={{ width: '100%' }} />
+                    </Row>
+                </Header>
+                <Content>
+                    <Row>
+                        Filter
+                    </Row>
+                    <List
+                        style={{ padding: '20px' }}
+                        grid={{ gutter: 16, column: 4 }}
+                        dataSource={data}
+                        renderItem={item => (
+                            <List.Item>
+                                <Card title={item.title}>Card content</Card>
+                            </List.Item>
+                        )}
+                    />
+                </Content>
+            </Layout>
+        </Layout>
+    );
+}
+
+export default Home;
