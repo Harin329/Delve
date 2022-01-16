204cd0b8ddaaeb4431eeb82e87875bfa7f8160df
Harin <master.harin@gmail.com>
2022-01-16 08:25:02 -0800

    profile images

--- frontend/src/screens/Profile.js
+++ frontend/src/screens/Profile.js
@@ -1,24 +1,25 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import '../App.css';
-import { Button, Typography, Layout, Input, List, Card, Row } from 'antd';
+import { Button, Typography, Layout, List, Row } from 'antd';
 
-import { useNavigate } from 'react-router-dom';
 import back from "../assets/back.png"
 import participant from "../assets/researcher.png"
 
 import axios from 'axios';
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth } from "firebase/auth";
+import { getStorage, ref, list, getDownloadURL } from "firebase/storage";
 
-const { Title, Paragraph, Text, Link } = Typography;
 
 function Profile() {
-    const { Header, Sider, Content } = Layout;
-    const { Search } = Input;
+
+    const { Title, Paragraph, Text, Link } = Typography;
+    const { Content } = Layout;
 
     const auth = getAuth();
+    const storage = getStorage();
     const user = auth.currentUser;
 
-    const [userID,setUserID] = useState(user.uid);
+    const [userID, setUserID] = useState(user.uid);
     const [data, setData] = useState([]);
     const [userResearcher, setUserResearcher] = useState(false);
     const [userName, setUserName] = useState('');
@@ -44,11 +45,6 @@ function Profile() {
         }
     }, [userID])
 
-    const navigate = useNavigate();
-    const backnav = () => {
-        navigate(-1)
-    }
-
     const getAllStudies = (UserResearcher, userID) => {
         try {
             const config = {
@@ -60,7 +56,35 @@ function Profile() {
             };
             axios(config).then((res) => {
                 console.log(res.data);
-                setData([...res.data]);
+                var finalData = []
+
+                res.data.forEach((study) => {
+                    const listRef = ref(storage, `Study/${study.study_id}`);
+
+                    list(listRef, { maxResults: 1 }).then((results) => {
+                        if (results.items.length > 0) {
+                            getDownloadURL(results.items[0]).then((url) => {
+                                finalData.push({ ...study, url: url })
+                            })
+                        } else {
+                            finalData.push({ ...study, url: '' })
+                        }
+
+                    })
+
+                    
+                })
+
+                function delay(time) {
+                    return new Promise(resolve => setTimeout(resolve, time));
+                  }
+                  
+                delay(1000).then(() => {
+                    console.log(finalData)
+                    setData(finalData)
+                });
+
+
             });
         } catch (e) {
             console.log(e);
@@ -73,27 +97,27 @@ function Profile() {
                 backgroundColor: "#FFFFFF",
             }}>
                 <img src={back} onClick={() => {
-                window.location.href = '/'
-            }} style={{
-                position: 'absolute',
-                left: 50,
-                top: 50,
-                height: 30,
-                width: 30,
-            }} />
+                    window.location.href = '/'
+                }} style={{
+                    position: 'absolute',
+                    left: 50,
+                    top: 50,
+                    height: 30,
+                    width: 30,
+                }} />
                 <Content style={{
                     width: '80%',
                     alignSelf: "center",
                     paddingTop: "2%",
                 }}>
-                       
+
                     <div style={{
                         marginLeft: 20,
                     }}>
                         <Row style={{
                             fontSize: 60,
                             fontWeight: 600,
-                        }}> 
+                        }}>
                             Hello {userName}
                         </Row>
                         <Row style={{
@@ -102,7 +126,7 @@ function Profile() {
                             Here are your studies...
                         </Row>
                     </div>
-                    
+
                     <List
                         style={{ padding: '20px' }}
                         grid={{ gutter: 0, column: 6 }}
@@ -111,23 +135,37 @@ function Profile() {
                             <List.Item onClick={() => {
                                 window.location.href = '/study/' + item.study_id
                             }}>
-                                <div style={{ 
-                                    padding: 5, 
+                                <div style={{
+                                    padding: 5,
                                     height: 200,
                                     width: 200,
-                                    marginTop: 20, 
-                                    borderRadius: '20px', 
-                                    position: 'relative' }}>
-                                        <img src={participant} style={{
-                                            backgroundColor: 'green', 
-                                            width: '100%', 
-                                            height: '100%', 
-                                            borderRadius: '20px'}}></img>
-                                        <h1 style={{
-                                            position: 'absolute', 
-                                            bottom: 10, 
-                                            left: 20}}> 
-                                        {item.title}</h1>
+                                    marginTop: 20,
+                                    borderRadius: '20px',
+                                    position: 'relative'
+                                }}>
+                                    <div style={{
+                                        width: '100%',
+                                        height: '100%',
+                                        borderRadius: '20px',
+                                        backgroundColor: 'black',
+                                        position: 'absolute',
+                                        opacity: 0.7
+                                    }}></div>
+                                    <img src={item.url} style={{
+                                        width: '100%',
+                                        height: '100%',
+                                        borderRadius: '20px',
+                                        objectFit: 'cover',
+                                    }}></img>
+                                    <Typography style={{
+                                        position: 'absolute',
+                                        bottom: 10,
+                                        left: 20,
+                                        color: 'white',
+                                        fontWeight: 'bold',
+                                         textAlign: 'left'
+                                    }}>
+                                        {item.title}</Typography>
                                 </div>
                             </List.Item>
                         )}
